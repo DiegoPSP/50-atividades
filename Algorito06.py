e11905de9120296c83b02c24a57aceeb7d1e7193
DiegoPSP <diego.paulo2022@gmail.com>
2023-12-15 02:16:24 -0300

    Criar um algoritmo que verifica se um número é primo.

---
+++ Algorito06.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um número e saiba se ele é primo: "))
+
+if numero % 2 == 0:
+    print(f"O numero {numero} não é primo!")
+else:
+    print(f"O numero {numero} é primo!")
