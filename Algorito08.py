542a1e079dfa4422d7c89e04f7574be395ef965b
DiegoPSP <diego.paulo2022@gmail.com>
2023-12-15 22:49:09 -0300

    Criar um algoritmo que conte quantas vezes uma palavra aparece em uma string.

---
+++ Algorito08.py
@@ -0,0 +1,6 @@
+palavras = 'O peito do pé de Pedro é preto. Quem disser que o peito do pé de Pedro é preto, tem o peito do pé mais preto do que o peito do pé de Pedro.'
+
+print(f'A palavra Pedro se repete {palavras.count("Pedro")} vezes;')
+print(f'A palavra Pé se repete {palavras.count("pé")} vezes;')
+print(f'A palavra Preto se repete {palavras.count("preto")} vezes.')
+print(f'A palavra Peito se repete {palavras.count("peito")} vezes.')
