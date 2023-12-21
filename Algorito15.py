2324158819c19280967ffe3bad03cafa4b3e1580
DiegoPSP <diego.paulo2022@gmail.com>
2023-12-20 23:25:33 -0300

    Algoritmo que verifica se um número é positivo, negativo ou zero.

---
+++ Algorito15.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número: "))
+if numero >= 1:
+    print(f"O número {numero} é positivo!")
+elif numero == 0:
+    print(f"O número {numero} é zero!")
+else:
+    print(f"O número {numero} é negativo!")
