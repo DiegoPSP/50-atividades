3f3e6338a55dd40e0a8b129d955078faa07bd15c
DiegoPSP <diego.paulo2022@gmail.com>
2024-02-17 12:53:09 -0300

    Update do código.

--- Algorito17.py
+++ Algorito17.py
@@ -1,6 +1,8 @@
+a = int(input("Digite um numero inteiro para calcular o MMC:"))
+b = int(input("Digite outro numero inteiro para calcular o MMC:"))
 def mmc(a,b):
     mmc = a
     while mmc % b:
         mmc += a
     return mmc
-print(mmc(456578451202, 4561231))
+print(mmc(a, b))
