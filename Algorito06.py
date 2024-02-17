bacd655c8aba9b77af0225bfd09d2feba27afce9
DiegoPSP <diego.paulo2022@gmail.com>
2024-02-17 15:37:12 -0300

    Correção do código.

--- Algorito06.py
+++ Algorito06.py
@@ -1,6 +1,14 @@
 numero = int(input("Digite um número e saiba se ele é primo: "))
-
-if numero % 2 == 0:
-    print(f"O numero {numero} não é primo!")
+contador = 0
+for c in range (1, numero + 1):
+    if numero % c == 0:
+        print('\033[33m', end=' ')
+        contador += 1
+    else:
+        print('\033[31m', end=' ')
+    print(f"{c}", end=' ')
+print(f"\n\033[mO numero {numero} foi divisível {contador} vezes")
+if contador == 2:
+    print('E por isso ele É PRIMO!')
 else:
-    print(f"O numero {numero} é primo!")
+    print('E por isso ele NÃO É PRIMO!')
