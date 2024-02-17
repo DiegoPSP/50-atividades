7a3738c973f9c17788eaa05f81676ecd9a63c7bc
DiegoPSP <diego.paulo2022@gmail.com>
2024-02-17 12:59:38 -0300

    Comentários adicionais.

--- Algorito18.py
+++ Algorito18.py
@@ -1,3 +1,3 @@
 import math
 numero = int(input("Digite um numero inteiro para saber a raiz quadrada dele: "))
-print(math.isqrt(numero))
+print(math.isqrt(numero))#Retorna a raíz quadrada inteira de (x). math.sqrt mesma coisa mas com ponto flutuante.
