022c0368e481bf4d2f1f4670461de39d5228852f
DiegoPSP <diego.paulo2022@gmail.com>
2024-02-18 14:23:50 -0300

    Criar um algoritmo que calcule o valor de uma potência (exponenciação).

---
+++ Algorito28.py
@@ -0,0 +1,8 @@
+def calcular_potencia(base, expoente):
+    return base ** expoente
+
+base = 2
+expoente = 3
+
+resultado = calcular_potencia(base, expoente)
+print(f"A base {base} elevado ao expoente {expoente} é igual a {resultado}")
